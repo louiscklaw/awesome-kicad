937fe7877bbb24a1af76de8b03157c09a0350072
Travis CI <travis@travis-ci.org>
2019-12-27 20:13:54 +0800

    init commit,

---
+++ temp.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+git submodule add --depth=1 https://github.com/louiscklaw/kicad-file-render-service.git
+git submodule add --depth=1 https://github.com/louiscklaw/kicad-automation-scripts.git
